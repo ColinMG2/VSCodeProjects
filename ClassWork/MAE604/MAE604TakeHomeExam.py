3bab63e437da5045bd3c808771180b405084ca1e
Colin Gerlach <colinmgerlach@gmail.com>
2025-03-20 09:57:37 -0400

    3/20/25 Added image files for the orbit plots to show that none of the planets can go through their respective lagrange points. I also made slight changes to the python script with comments to make the code more readable.

--- ClassWork/MAE604/MAE604TakeHomeExam.py
+++ ClassWork/MAE604/MAE604TakeHomeExam.py
@@ -3,20 +3,23 @@ import numpy as np
 import pandas as pd
 import chardet
 
-
+# Checling the encoding of the csv file
 with open('Lagrange Sol.csv', 'rb') as f:
     result = chardet.detect(f.read())  
     print(result['encoding'])
 
+# Reading the data from the csv file and printing the columns
 data = pd.read_csv('Lagrange Sol.csv', encoding='windows-1252')
 print(data)
 print(data.columns)
 
+# Extracting the data from the csv file using the column names
 planet_smas = data['semimajor_axis(xe9m)']*1e9
 L1_values = data['L1(xe9m)']*1e9
 L2_values = data['L2(xe9m)']*1e9
 L3_values = data['L3(xe9m)']*1e9
 
+# Defining the planet names in simpler list than csv file
 planet_names = [
     'Mercury',
     'Venus',
@@ -28,6 +31,7 @@ planet_names = [
     'Neptune'
 ]
 
+# Creating a plot
 plt.figure(figsize=(10, 10))
 
 # Plotting the orbits
@@ -44,6 +48,7 @@ for L1, L2, L3, name in zip(L1_values, L2_values, L3_values, planet_names):
     plt.plot(L2, 0, 'o', label='L2 for ' + name)
     plt.plot(L3, 0, '.', label='L3 for ' + name)
 
+# Adding labels and legend
 plt.title('Solar System Circular Orbits')
 plt.axis('equal')
 plt.xlabel('x [m]')
