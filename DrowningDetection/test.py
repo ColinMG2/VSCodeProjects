01bf05fe40686204e8dc3300b746ff4ec8a92ca4
Colin Gerlach <colinmgerlach@gmail.com>
2025-03-25 18:32:23 -0400

    3/25/25 Created custom best.pt model for YOLOv11 and reorganized the workspace by putting the models in a separate folder.

--- DrowningDetection/test.py
+++ DrowningDetection/test.py
@@ -1,7 +1,21 @@
 from ultralytics import YOLO
+import cv2
 
 # Load model
-model = YOLO('yolo11m-pose.pt')
+model = YOLO('yolomodels/best.pt')
 
 # Results
-results = model.track(source='videos\shahriar_drowning.mp4', show=True)
+input_source = input('Enter the path to the video: ')
+results = model.predict(source=input_source, stream=True)
+
+cv2.namedWindow('results', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('results', 800, 600)
+
+# Loop through the results and display them
+for result in results:
+    frame = result.plot()  # Get the frame with predictions drawn
+    cv2.imshow('results', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+        break
+
+cv2.destroyAllWindows()
